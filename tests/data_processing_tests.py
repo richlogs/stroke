9cf5fca354bcaa3d78f5a532b749efe6eb70f963
Richlogs <richcostello@outlook.co.nz>
2023-11-25 12:46:25 +1300

    chore: remove additional train_test_split function

--- tests/data_processing_tests.py
+++ tests/data_processing_tests.py
@@ -4,37 +4,6 @@ import pandas as pd
 from utils.data_processing import train_test_split
 
 
-def train_test_split(
-    df, stratify_col=None, test_size=0.2, random_state=None, stratify=True
-):
-    """
-    Perform stratified or non-stratified random sampling on a DataFrame.
-
-    Parameters:
-    df (pandas.DataFrame): The DataFrame to sample from.
-    stratify_col (str): The column to use for stratification.
-    test_size (float): The proportion of the dataset to include in the test split.
-    random_state (int): The seed used by the random number generator.
-    stratify (bool): Whether to perform stratified sampling.
-
-    Returns:
-    pandas.DataFrame, pandas.DataFrame: The training set and the test set.
-    """
-    if stratify and stratify_col is not None:
-        test = df.groupby(stratify_col).apply(
-            lambda x: x.sample(frac=test_size, random_state=random_state)
-        )
-        test.index = test.index.droplevel(0)
-        train = df.drop(test.index)
-    else:
-        test = df.sample(frac=test_size, random_state=random_state)
-        train = df.drop(test.index)
-
-    train = train.sample(frac=1, random_state=random_state).reset_index(drop=True)
-    test = test.sample(frac=1, random_state=random_state).reset_index(drop=True)
-    return train, test
-
-
 def training_split_tests(
     df: pd.DataFrame, col: str, iterations: int, stratify_col="stroke"
 ):
