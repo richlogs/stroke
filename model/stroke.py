88d959a4f82f6922cab21d5a74e23c99b816443a
Richlogs <richcostello@outlook.co.nz>
2023-11-25 23:41:11 +1300

    feat: add mode impute function

--- model/stroke.py
+++ model/stroke.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
+from utils.data_processing import mode_impute
 from utils.data_processing import one_hot_encode
 
 
 data = pd.read_csv("data/stroke.csv", delimiter=",")
 data.drop(columns=["id"], inplace=True)
+print(data["smoking_status"].value_counts())
+data = mode_impute(data, "smoking_status", "Unknown")
+print(data["smoking_status"].value_counts())
 
 one_hot = one_hot_encode(data)
 x = 3
--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,7 +1,9 @@
+from typing import Tuple
+
 import pandas as pd
 
 
-def encode_categories(data: pd.DataFrame) -> pd.DataFrame:
+def encode_categories(df: pd.DataFrame) -> pd.DataFrame:
     """
     Encode categorical columns of a DataFrame.
 
@@ -11,12 +13,12 @@ def encode_categories(data: pd.DataFrame) -> pd.DataFrame:
     Returns:
     pandas.DataFrame: The encoded DataFrame.
     """
-    for column in data.select_dtypes(include="object"):
-        data[column] = pd.factorize(data[column])[0]
-    return data
+    for column in df.select_dtypes(include="object"):
+        df[column] = pd.factorize(df[column])[0]
+    return df
 
 
-def one_hot_encode(data: pd.DataFrame) -> pd.DataFrame:
+def one_hot_encode(df: pd.DataFrame) -> pd.DataFrame:
     """
     One-hot encode categorical columns of a DataFrame.
 
@@ -26,12 +28,16 @@ def one_hot_encode(data: pd.DataFrame) -> pd.DataFrame:
     Returns:
     pandas.DataFrame: The encoded DataFrame.
     """
-    return pd.get_dummies(data, drop_first=True, dtype=int)
+    return pd.get_dummies(df, drop_first=True, dtype=int)
 
 
 def train_test_split(
-    df, stratify_col=None, test_size=0.2, random_state=None, stratify=True
-):
+    df: pd.DataFrame,
+    stratify_col: str | None = None,
+    test_size: float = 0.2,
+    random_state: int | None = None,
+    stratify: bool = True,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """
     Perform stratified or non-stratified random sampling on a DataFrame.
 
@@ -58,3 +64,24 @@ def train_test_split(
     train = train.sample(frac=1, random_state=random_state).reset_index(drop=True)
     test = test.sample(frac=1, random_state=random_state).reset_index(drop=True)
     return train, test
+
+
+def mode_impute(df: pd.DataFrame, col: str, value: str | None) -> pd.DataFrame:
+    """
+    Impute missing values in a column with the mode.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame to impute.
+    col (str): The column to impute.
+    value (str): The value to replace with the mode.
+
+    Returns:
+    pandas.DataFrame: The imputed DataFrame.
+    """
+    mode = df[col].mode().values[0]
+
+    if value:
+        df[col].replace(value, mode, inplace=True)
+    else:
+        df[col].fillna(mode, inplace=True)
+    return df
