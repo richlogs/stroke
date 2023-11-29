6c639b6e78fae3579beba0d0a1f6a0aff1ea3b57
Richlogs <richcostello@outlook.co.nz>
2023-11-29 18:47:17 +1300

    chore: explore encoding options

--- model/stroke.py
+++ model/stroke.py
@@ -6,6 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 from utils.data_processing import one_hot_encode
 from utils.data_processing import train_test_split
+from utils.imputation import mean_impute
 from utils.imputation import mode_impute
 
 seed = 50
@@ -16,7 +17,8 @@ data = pd.read_csv("data/stroke.csv", delimiter=",")
 ## Preprocess
 data.drop(columns=["id"], inplace=True)
 data = mode_impute(data, "smoking_status", "Unknown")
-one_hot = one_hot_encode(data)
+data = mean_impute(data, col="bmi")
+one_hot = one_hot_encode(data, drop_first=False)
 
 ## Train test split
 train, test = train_test_split(
@@ -44,6 +46,7 @@ best_score = grid_search.best_score_
 clf_auc = DecisionTreeClassifier(
     max_depth=best_params["max_depth"],
     min_samples_split=best_params["min_samples_split"],
+    random_state=seed,
 )
 clf_auc.fit(X_train, y_train)
 
@@ -52,13 +55,13 @@ y_pred = clf.predict(X_test)
 y_pred_auc = clf_auc.predict(X_test)
 acc = sum(y_test == y_pred) / len(y_pred)
 acc_auc = sum(y_test == y_pred_auc) / len(y_pred_auc)
-print(f"Accuracy standard DT: {round(acc * 100, 4)}%")
-print(f"Accuracy aucpr DT: {round(acc_auc * 100, 4)}%")
+print(f"Accuracy DT standard: {round(acc * 100, 4)}%")
+print(f"Accuracy DT aucpr: {round(acc_auc * 100, 4)}%")
 
 ## AUCPR
 y_prob = clf.predict_proba(X_test)[:, 1]
 y_prob_auc = clf_auc.predict_proba(X_test)[:, 1]
 aucpr_score = average_precision_score(y_test, y_prob)
 aucpr_score_auc = average_precision_score(y_test, y_prob_auc)
-print(f"AUCPR standard DT: {round(aucpr_score * 100, 4)}%")
-print(f"AUCPR aucpr DT: {round(aucpr_score_auc * 100, 4)}%")
+print(f"AUCPR DT standard: {round(aucpr_score * 100, 4)}%")
+print(f"AUCPR DT aucpr: {round(aucpr_score_auc * 100, 4)}%")
